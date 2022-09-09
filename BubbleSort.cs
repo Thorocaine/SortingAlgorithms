042fe7cdc87bb623d5494e5a89f9c35aa7d6aa0f
Jonathan Peel <me@jonathanpeel.co.za>
2022-09-09 07:31:10 +0300

    Feat: Add Bubble Sort

---
+++ BubbleSort.cs
@@ -0,0 +1,16 @@
+﻿namespace SortingAlgorithms;
+
+public class BubbleSort
+{
+    static void Swap (IList<int> array, int a, int b) =>
+        (array[a], array[b]) = (array[b], array[a]);
+
+    public static void Sort(int[] array)
+    {
+        var n = array.Length;
+        for (var i = 0; i < n - 1; i++)
+            for (var j = 0; j < n - i - 1; j++)
+                if (array[j] > array[j + 1])
+                    Swap(array, j, j + 1);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using SortingAlgorithms;
+
+var array = new [] {10,9,8,7,6,5,4,3,2,1};
+BubbleSort.Sort(array);
+foreach (var i in array)
+    Console.WriteLine(i);
