eb5c924c85d4e564eb535d88b4c81fca6a5994e0
Jonathan Peel <me@jonathanpeel.co.za>
2022-09-09 18:32:55 +0300

    feat: Add selection sort

--- BubbleSort.cs
+++ BubbleSort.cs
@@ -1,9 +1,8 @@
 ﻿namespace SortingAlgorithms;
 
-public class BubbleSort
+public static class BubbleSort
 {
-    static void Swap (IList<int> array, int a, int b) =>
-        (array[a], array[b]) = (array[b], array[a]);
+    
 
     public static void Sort(int[] array)
     {
@@ -11,6 +10,6 @@ public class BubbleSort
         for (var i = 0; i < n - 1; i++)
             for (var j = 0; j < n - i - 1; j++)
                 if (array[j] > array[j + 1])
-                    Swap(array, j, j + 1);
+                    Common.Swap(array, j, j + 1);
     }
 }
---
+++ Common.cs
@@ -0,0 +1,7 @@
+﻿namespace SortingAlgorithms;
+
+public static class Common
+{
+    public static void Swap (IList<int> array, int a, int b) =>
+        (array[a], array[b]) = (array[b], array[a]);
+}
