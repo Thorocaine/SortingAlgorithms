0dbee11eeb8f5b8d4344d43a69e097eccf4baeb1
Jonathan Peel <me@jonathanpeel.co.za>
2022-09-12 09:05:41 +0300

    change: trying to write quick sort

---
+++ QuickSort.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Concurrent;
+
+namespace SortingAlgorithms;
+
+public static class QuickSort
+{
+    public enum Pivot
+    {
+        First,
+        Middle,
+        Last,
+    } //, Last, MedianOfThree, Random } 
+
+    delegate int Partition(Span<int> array); 
+
+    public static void Sort(int[] array, Pivot pivot)
+    {
+        quickSort(array, GetPartition(pivot));
+    }
+
+    static int PartitionMiddle(Span<int> array)
+    {
+        int left = 0; int right = array.Length - 1;
+        int i = left, j = right;
+
+        int tmp;
+
+        var pivot = array[(left + right) / 2];
+
+
+        while (i <= j)
+        {
+            while (array[i] < pivot)
+
+                i++;
+
+            while (array[j] > pivot)
+
+                j--;
+
+            if (i <= j)
+            {
+                tmp = array[i];
+
+                array[i] = array[j];
+
+                array[j] = tmp;
+
+                i++;
+
+                j--;
+            }
+        }
+
+        
+
+
+        return i;
+    }
+
+    static int PartitionLast(Span<int> array)
+    {
+        int start = 0; int end = array.Length - 1;
+            int pivot = array[end]; // pivot element  
+            int i = (start - 1);  
+  
+            for (int j = start; j <= end - 1; j++)  
+            {  
+                // If current element is smaller than the pivot  
+                if (array[j] < pivot)  
+                {  
+                    i++; // increment index of smaller element  
+                    (array[i], array[j]) = (array[j], array[i]);
+                }  
+            }  
+            (array[i+1], array[end]) = (array[end], array[i+1]);
+            return (i + 1);  
+        
+    }
+    
+    static void quickSort(Span<int> array, Partition partition)
+    {
+        var index = partition(array);
+        if (index > 1)
+        {
+            var leftArray = array[..index];
+            quickSort(leftArray, partition);
+        }
+
+        if (index < array.Length - 1)
+        {
+            var rightArray = array[(index+1)..];
+            quickSort(rightArray, partition);
+        }
+    }
+
+    public static IEnumerable<int> LazyQuickSort(this IEnumerable<int> list)
+    {
+        if (!list.Skip(1).Any()) return list;
+        var pivot = list.First();
+        var left = list.Skip(1).Where(x => x < pivot).LazyQuickSort();
+        var right = list.Skip(1).Where(x => x >= pivot).LazyQuickSort();
+        return left.Concat(new[] {pivot}).Concat(right);
+    }
+
+    static Partition GetPartition(Pivot pivot)
+    {
+        return pivot switch
+        {
+            Pivot.Middle => PartitionMiddle,
+            Pivot.Last => PartitionLast,
+            _ => PartitionMiddle,
+        };
+    }
+}
--- SortingTests.cs
+++ SortingTests.cs
@@ -47,4 +47,36 @@ public class SortingTests
         MergeSort.Sort(_array);
         Assert.Equal(_ordered, _array);
     }
+
+    [Fact]
+    public void QuickSortLazyTest()
+    {
+        var sorted = _array.LazyQuickSort().ToArray();
+        Assert.Equal(_ordered, sorted);
+    }
+
+    [Fact]
+    public void QuickSortPivotOnFirstTest()
+    {
+        var uns = new [] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        QuickSort.Sort(uns, QuickSort.Pivot.First);
+        Assert.Equal(new [] {1, 2, 3, 4, 5, 6, 7, 8,9, 10}, uns);
+    }
+
+    [Fact]
+    public void QuickSortPivotOnMiddleTest()
+    {
+        var uns = new [] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        QuickSort.Sort(uns, QuickSort.Pivot.Middle);
+        Assert.Equal(new [] {1, 2, 3, 4, 5, 6, 7, 8,9, 10}, uns);
+    }
+
+    [Fact]
+    public void QuickSortPivotOnLastTest()
+    {
+        var uns = new [] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        QuickSort.Sort(uns, QuickSort.Pivot.First);
+        Assert.Equal(new [] {1, 2, 3, 4, 5, 6, 7, 8,9, 10}, uns);
+    }
+    
 }
